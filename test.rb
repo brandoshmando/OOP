97504c31d141d038065b9094546a85e5a54cac54
Brandon Craft <brancraft@gmail.com>
2014-06-04 23:30:34 -0400

    First attempt at Rover answer. Still throwing errors...

--- test.rb
+++ test.rb
@@ -1,8 +1,11 @@
 class Rover
-	def initialize(x, y, bearing)
+	attr_accessor :x, :y
+	attr_reader :d
+	
+	def initialize(x, y, d)
 		@x = x
 		@y = y
-		@bearing = bearing
+		@d = d
 	end
 	
 	def execute_commands(commands)
@@ -21,6 +24,44 @@ class Rover
 	end
 
 	def change_direction(rotate)
-		bearings = ["N", "E", "S", "W"] 
+		cardinal = ["N", "E", "S", "W"]
+		if rotate == "R"
+			@d = cardinal[cardinal.index(:d) += 1]
+		else
+			@d = cardinal[(cardinal.index(:d) -= 1)]
+		end
 	end
+
+	def move
+		if @x > @@x || @x < 0
+			puts "Error: Rover cannot leave the boundaries of the plateau! Please adjust direction before moving."
+		elsif @y > @@y || @y < 0
+			puts "Error: Rover cannot leave the boundaries of the plateau! Please adjust direction before moving."
+		else
+			case @d 
+			when "N"
+				@y += 1
+			when "E"
+				@x += 1
+			when "S"
+				@y -= 1
+			when "W"
+				@x -= 1
+			end
+		end
+	end
+
+	def self.plateau_boundaries(str)
+		@@x = str[0]
+		@@y = str[2]
+	end
+end	
+
+Rover.plateau_boundaries('5 5')
+
+rover1 = Rover.new('1 2 N')
+rover1.execute_commands('LMLMLMLMM')
+
+rover2 = Rover.new('3 3 E')
+rover2.execute_commands('MMRMMRMRRM')
 
